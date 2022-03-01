bd10f82823a8037458b7e49a8cda72f1a2cc0c74
Kerstin Hung <tenwu06@gmail.com>
2022-03-01 11:44:29 +0800

    html practice-document and website structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
 var myImage = document.querySelector('img');
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
 
 myImage.onclick = function(){
     let mySrc = myImage.getAttribute('src');
@@ -9,25 +7,4 @@ myImage.onclick = function(){
     }else{
         myImage.setAttribute('src', 'images/orangeMonster.png');
     }
-}
-
-myButton.onclick = function(){
-    setUserName();
-}
-
-function setUserName(){
-    let myName = prompt('Please enter your name'); /* prompt 有點像 alert, 但它會要求使用者輸入文字*/
-    if(!myName || myName == null){
-        setUserName();
-    }else{
-        localStorage.setItem('name', myName);
-        myHeading.innerHTML = 'Henlo, ' + myName;
-    }
-}
-
-if(!localStorage.getItem('name')){
-    setUserName();
-}else{
-    let storedName = localStorage.getItem('name');
-    myHeading.innerHTML = 'Henlo, ' + storedName;
 }
